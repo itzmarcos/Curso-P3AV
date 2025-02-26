6f413dc3f92c790bdff0e2fb9c5c76cfb88f2041
itzmarcos <marcozfuwa@gmail.com>
2025-02-26 07:48:55 -0300

    pyside att

--- Praticas/Pillow.py
+++ Praticas/Pillow.py
@@ -1,24 +1,24 @@
-from pathlib import Path
+# from pathlib import Path
 
-from PIL import Image
+# from PIL import Image
 
-ROOT_FOLDER = Path(__file__).parent
-ORIGINAL = ROOT_FOLDER / 'oldphoto'
-NEW_IMAGE = ROOT_FOLDER / 'newphoto'
+# ROOT_FOLDER = Path(__file__).parent
+# ORIGINAL = ROOT_FOLDER / 'oldphoto'
+# NEW_IMAGE = ROOT_FOLDER / 'newphoto'
 
-pil_image = Image.open(ORIGINAL)
-width, height = pil_image.size
-exif = pil_image.info['exif']
+# pil_image = Image.open(ORIGINAL)
+# width, height = pil_image.size
+# exif = pil_image.info['exif']
 
-# width     new_width
-# height    ??
-new_width = 640
-new_height = round(height * new_width / width)
+# # width     new_width
+# # height    ??
+# new_width = 640
+# new_height = round(height * new_width / width)
 
-new_image = pil_image.resize(size=(new_width, new_height))
-new_image.save(
-    NEW_IMAGE,
-    optimize=True,
-    quality=70,
-    # exif=exif,
-)
+# new_image = pil_image.resize(size=(new_width, new_height))
+# new_image.save(
+#     NEW_IMAGE,
+#     optimize=True,
+#     quality=70,
+#     # exif=exif,
+# )
