a1ad7d652467f885752041aacfd094847a9fe473
itzmarcos <marcozfuwa@gmail.com>
2025-04-11 08:40:20 -0300

    sql

--- Praticas/SQL/main.py
+++ Praticas/SQL/main.py
@@ -88,3 +88,24 @@
  
 #      cursor.close()
 #      connection.close()
+
+
+
+
+
+
+
+
+# import pymysql
+ 
+#  connection = pymysql.connect(
+#      host='localhost',
+#      user='usuario',
+#      password='senha',
+#      database='base_de_dados',
+#  )
+ 
+#  with connection:
+#      with connection.cursor() as cursor:
+#          # SQL
+#         print(cursor)
